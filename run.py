29672dfffce371f61319197a042f0eddc8fbb845
vihanga <7277860+vihanga@users.noreply.github.com>
2023-06-20 14:47:25 +0100

    update

---
+++ run.py
@@ -0,0 +1,5 @@
+from rnai.main import RNAI
+
+rnai = RNAI()
+
+rnai.initialise_vertical('methane removal from ambient air', list_of_publications = ['Atmospheric methane removal: a research agenda', 'A novel green technology: Reducing carbon dioxide and eliminating methane from the atmosphere', 'Methane removal and atmospheric restoration', 'New Directions: Atmospheric methane removal as a way to mitigate climate change?', 'Methane removal seen as tool to slow warming'])
