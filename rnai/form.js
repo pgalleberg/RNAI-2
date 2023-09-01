8193beb25d15b09ec43b13107f14247952ba19f0
Syed Hassaan Hasan <hassaanhasan95@hotmail.com>
2023-09-01 19:18:28 +0500

    add auto complete feature

---
+++ rnai/form.js
@@ -0,0 +1,8 @@
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+<script>
+  hbspt.forms.create({
+    region: "na1",
+    portalId: "20408997",
+    formId: "b164c0da-fa7a-4137-9f65-97235e497794"
+  });
+</script>
