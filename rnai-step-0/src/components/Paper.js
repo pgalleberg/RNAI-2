8f9e8201c3316378e30f626fcce62fdfcff74fcd
Ubuntu <ubuntu@ip-172-31-30-79.us-west-1.compute.internal>
2023-11-25 19:33:09 +0000

    create links to get paper details

--- rnai-step-0/src/components/Paper.js
+++ rnai-step-0/src/components/Paper.js
@@ -6,43 +6,43 @@ import { faUnlock } from '@fortawesome/free-solid-svg-icons'
 const Paper = ({ paperDetails, index }) => {
 
     console.log("Paper::paperDetails: ", paperDetails)
-    function expand(index, section){
+    function expand(index, section) {
         console.log("index received: ", index)
         console.log("section name: ", section)
-    
+
         let className = ''
-        if (section === 'citations'){
-          className = 'citations'
-        } else if (section === 'references'){
-          className = 'references'
+        if (section === 'citations') {
+            className = 'citations'
+        } else if (section === 'references') {
+            className = 'references'
         }
-        const buttonText = document.getElementsByClassName('expand-'+section)[index].innerHTML
-        
-        if (buttonText === 'Expand'){
-          document.getElementsByClassName(className)[index].style.display = 'block'
-          document.getElementsByClassName('expand-'+section)[index].innerHTML = 'Collapse'
-        } else if (buttonText === 'Collapse'){
-          document.getElementsByClassName(className)[index].style.display = 'none'
-          document.getElementsByClassName('expand-'+section)[index].innerHTML = 'Expand'
+        const buttonText = document.getElementsByClassName('expand-' + section)[index].innerHTML
+
+        if (buttonText === 'Expand') {
+            document.getElementsByClassName(className)[index].style.display = 'block'
+            document.getElementsByClassName('expand-' + section)[index].innerHTML = 'Collapse'
+        } else if (buttonText === 'Collapse') {
+            document.getElementsByClassName(className)[index].style.display = 'none'
+            document.getElementsByClassName('expand-' + section)[index].innerHTML = 'Expand'
         }
-       
+
         console.log("Expand clicked")
     }
 
     return (
-            <div>
-                <Link to={`/paper/${paperDetails._id}`}><h2>{paperDetails.title}</h2></Link>
+        <div>
+            <Link to={`/paper/${paperDetails.paperId}`}><h2>{paperDetails.title}</h2></Link>
 
-                <div className="authors">
+            <div className="authors">
                 {paperDetails.authors.map((author) => (
                     <>
-                    <Link to={`/author/${author.authorId}`}><span className='author'>{author.name}</span></Link>
-                    <span>&nbsp;&nbsp;</span>
+                        <Link to={`/author/${author.authorId}`}><span className='author'>{author.name}</span></Link>
+                        <span>&nbsp;&nbsp;</span>
                     </>
                 ))}
-                </div>
+            </div>
 
-                <>
+            <>
                 {paperDetails.s2FieldsOfStudy.map((field) => (
                     field.source === 's2-fos-model' && <span className="detail">{field.category}</span>
                 ))}
@@ -51,68 +51,70 @@ const Paper = ({ paperDetails, index }) => {
 
                 <span className="detail">{paperDetails.publicationDate}</span>
                 <br></br>
-                </>
+            </>
 
-                {paperDetails.tldr && 
+            {paperDetails.tldr &&
                 <>
                     <span className="text">TLDR</span>
                     <p style={{ display: 'inline', margin: 0 }}>{paperDetails.tldr.text}</p>
                     <br></br>
                 </>
-                }
+            }
 
-                {paperDetails.abstract && 
+            {paperDetails.abstract &&
                 <>
                     <span className="text">Abstract</span>
                     <p style={{ display: 'inline', margin: 0 }}>{paperDetails.abstract}</p>
                     <br></br>
                 </>
-                }
+            }
 
-                {paperDetails.isOpenAccess &&
+            {paperDetails.isOpenAccess &&
                 <div className='pdf'>
                     <br></br>
-                    <FontAwesomeIcon icon={faUnlock} style={{marginRight: '10px'}}/>
+                    <FontAwesomeIcon icon={faUnlock} style={{ marginRight: '10px' }} />
                     <a href={paperDetails.openAccessPdf && paperDetails.openAccessPdf.url} target="_blank">PDF</a>
                 </div>
-                }
+            }
 
-                <>
+            <>
                 {paperDetails.citations &&
                     <>
-                    <br></br>
-                    <span>{paperDetails.citationCount}&nbsp;Citations&nbsp;</span>
-                    <span>({paperDetails.influentialCitationCount}&nbsp;Influential Citations)&nbsp;</span>
-                    <span className="expand-citations" onClick={() => expand(index, 'citations')}>Expand</span>
-                    <div className="citations" style={{display: 'none'}}>
                         <br></br>
-                        {paperDetails.citations.map((citation) => (
-                            <a href='#'>&nbsp;&nbsp;•&nbsp;&nbsp;{citation.title}</a>
-                        ))}
-                        {/* <span className="expand-citations" onClick={() => expand(index)}>Expand</span> */}
-                    </div>
+                        <span>{paperDetails.citationCount}&nbsp;Citations&nbsp;</span>
+                        <span>({paperDetails.influentialCitationCount}&nbsp;Influential Citations)&nbsp;</span>
+                        <span className="expand-citations" onClick={() => expand(index, 'citations')}>Expand</span>
+                        <div className="citations" style={{ display: 'none' }}>
+                            <br></br>
+                            {paperDetails.citations.map((citation) => (
+                                <Link to={`/paper/${citation.paperId}`}>&nbsp;&nbsp;•&nbsp;&nbsp;{citation.title}</Link>
+                                // <a href='#'>&nbsp;&nbsp;•&nbsp;&nbsp;{citation.title}</a>
+                            ))}
+                            {/* <span className="expand-citations" onClick={() => expand(index)}>Expand</span> */}
+                        </div>
                     </>
                 }
-                </>
+            </>
 
-                <>
+            <>
                 {paperDetails.references &&
                     <>
-                    <br></br>
-                    <span>{paperDetails.references.length}&nbsp;References&nbsp;</span>
-                    <span className="expand-references" onClick={() => expand(index, 'references')}>Expand</span>
-                    <div className="references" style={{display: 'none'}}>
                         <br></br>
-                        {paperDetails.references.map((reference) => (
-                            <a href='#'>&nbsp;&nbsp;•&nbsp;&nbsp;{reference.title}</a>
-                        ))}
-                        {/* <span className="expand-references" onClick={() => expand(index)}>Expand</span> */}
-                    </div>
+                        <span>{paperDetails.references.length}&nbsp;References&nbsp;</span>
+                        <span className="expand-references" onClick={() => expand(index, 'references')}>Expand</span>
+                        <div className="references" style={{ display: 'none' }}>
+                            <br></br>
+                            {paperDetails.references.map((reference) => (
+                                <Link to={`/paper/${reference.paperId}`}>&nbsp;&nbsp;•&nbsp;&nbsp;{reference.title}</Link>
+                                // <a href='#'>&nbsp;&nbsp;•&nbsp;&nbsp;{reference.title}</a>
+                            ))}
+                            {/* <span className="expand-references" onClick={() => expand(index)}>Expand</span> */}
+                        </div>
                     </>
                 }
-                </>
+            </>
 
-            </div>
+        </div>
     )
 }
 
