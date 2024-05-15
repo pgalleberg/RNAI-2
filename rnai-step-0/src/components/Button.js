51f4d2d3eabaa66096d601cc6b46d7d03bfd167d
Syed Hassaan Hasan <hassaanhasan95@hotmail.com>
2024-05-15 22:22:46 +0500

    update python requirements and delete rnai-step-0

--- rnai-step-0/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ text, className, onClick }) => {
-  return (
-    <>
-        <button className={className} onClick={onClick} type="button">{text}</button>
-    </>
-  )
-}
-
-export default Button
